3c4bb473cbefe1c1bcc97d9458d40cfc78e91dd9
Aleksei <vasilliev@gmail.com>
2022-09-23 11:20:59 +0500

    initiation commit++

--- Program.cs
+++ Program.cs
@@ -5,12 +5,13 @@
 Console.Clear();
 Console.WriteLine("\n Задача 25");
 Console.WriteLine("Введите числа А и B: "); // Приглашение ко вводу
-double a = int.Parse(Console.ReadLine());
-double b = int.Parse(Console.ReadLine());
+double a = Math.Abs(int.Parse(Console.ReadLine() ?? string.Empty)); // int columns = Math.Abs(int.Parse(Console.ReadLine() ?? string.Empty)); - алгоритм избавления от ошибки null
+// int num = int.Parse(Console.ReadLine());
+double b = Math.Abs(int.Parse(Console.ReadLine() ?? string.Empty)); // int columns = Math.Abs(int.Parse(Console.ReadLine() ?? string.Empty)); - алгоритм избавления от ошибки null
+// int num = int.Parse(Console.ReadLine());
 
 void Degree(){
   double degree = Math.Pow(a, b);
   Console.Write($"число {a} в степени {b} равно {degree}");
 }
-
 Degree();
